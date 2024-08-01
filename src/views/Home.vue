75c21d35c0b90975ebd84d1bfdedaae91f728c6a
周锦弘 <zhoujh@wp.com>
2024-08-01 09:59:37 +0800

    2024/8/1更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <h1>欢迎来到我的AI技术学习网站</h1>
-    <div v-html="articleContent"></div>
+    <h1>欢迎来到我的AI技术网站</h1>
   </div>
 </template>
 
 <script>
-import articleContent from '@/assets/data/articles/PromptEngineeringGuide/PromptEngineeringGuide.html';
-
 export default {
-  name: 'HomeComponent',
-  data() {
-    return {
-      articleContent
-    }
-  }
+  name: 'HomeComponent'
 }
 </script>
