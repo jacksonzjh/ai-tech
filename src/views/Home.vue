b58650839b94c8db0068ab2cadcddd9bd44a1f0b
周锦弘 <zhoujh@wp.com>
2024-08-05 18:18:05 +0800

    主页调整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 使用 v-html 指令将 HTML 内容直接插入页面 -->
     <div v-html="projectReadMeContent"></div>
   </div>
 </template>
@@ -17,10 +18,15 @@ export default {
   },
   methods: {
     fetchProjectReadMe() {
-      fetch('/ProjectReadMe.html')
-        .then(response => response.text())
-        .then(html => {
-          this.projectReadMeContent = html;
+      fetch(`${process.env.BASE_URL}ProjectReadMe.html`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('加载 ProjectReadMe 失败');
+          }
+          return response.text();
+        })
+        .then(htmlContent => {
+          this.projectReadMeContent = htmlContent;
         })
         .catch(error => console.error('加载 ProjectReadMe 时出错:', error));
     }
