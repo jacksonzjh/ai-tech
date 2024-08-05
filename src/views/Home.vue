9a4180a5babe50bf5b8bf645243cb355d089ec2a
周锦弘 <zhoujh@wp.com>
2024-08-05 18:09:45 +0800

    主页完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    <h1>欢迎来到我的AI技术学习网站</h1>
+    <div v-html="projectReadMeContent"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeComponent'
+  name: 'HomeComponent',
+  data() {
+    return {
+      projectReadMeContent: ''
+    }
+  },
+  mounted() {
+    this.fetchProjectReadMe();
+  },
+  methods: {
+    fetchProjectReadMe() {
+      fetch('/ProjectReadMe.html')
+        .then(response => response.text())
+        .then(html => {
+          this.projectReadMeContent = html;
+        })
+        .catch(error => console.error('加载 ProjectReadMe 时出错:', error));
+    }
+  }
 }
 </script>
